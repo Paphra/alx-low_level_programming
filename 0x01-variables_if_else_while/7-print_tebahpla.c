30eb0d1b5942eb9f1a197b4ae8f008636f392da0
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-16 05:48:32 -0700

    Print alphabets in reverse lowercase end with new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets in reverse order
+ *
+ * Return: 0 - success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
