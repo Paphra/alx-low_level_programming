62b4c3a3f5d0956892b9ac17ee41d0bddb3a464b
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-16 05:33:51 -0700

    Print all single digit numbers of base 10 starting from 0. No char variables, only use putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print all single digit numbers from 0
+ * main - Print all single digit numbers from 0 - no char and only putchar
  *
  * Return: 0 - success
  */
@@ -10,7 +10,7 @@ int main(void)
 	int number;
 
 	for (number = 0; number < 10; number++)
-		printf("%d", number);
+		putchar((char)number);
 
 	putchar('\n');
 	return (0);
