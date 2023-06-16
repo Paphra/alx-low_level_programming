ad9e19e668590385eabc9d9ef36a51050918beb7
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-16 07:28:55 -0700

    Print all possible two two-digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of two two-digit numbers
+ *
+ * Return: 0 - success
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = k; l < 10; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l);
+					if (j < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
