7f4dc793ba9b24afd72bc8f5a78b973aa156483b
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-16 21:40:18 -0700

    Had removed this file - 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers from 0
+ *
+ * Return: 0 - success
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d\n", number);
+
+	return (0);
+}
