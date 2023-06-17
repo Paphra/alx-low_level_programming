34c4ba3e06d1763765b4e7ff115f6c3de0561065
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-16 19:27:40 -0700

    Edit 102 - 005

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,32 +9,33 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
+	int d1i;
+	int d2i;
+	int d1j;
+	int d2j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i; j < 10; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			for (k = j; k < 10; k++)
+			d1i = i / 10;
+			d2i = i % 10;
+			d1j = j / 10;
+			d2j = j % 10;
+
+			putchar('0' + d1i);
+			putchar('0' + d2i);
+			putchar(' ');
+			putchar('0' + d1j);
+			putchar('0' + d2j);
+
+			if (i != 98)
 			{
-				for (l = k; l < 10; l++)
-				{
-					if (k == 0)
-						l = k + 1;
-					putchar('0' + i);
-					putchar('0' + j);
-					putchar(' ');
-					putchar('0' + k);
-					putchar('0' + l);
-					if (i != 9 && j != 8)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(',');
+				puchar(' ');
 			}
 		}
+
 	}
 
 	putchar('\n');
