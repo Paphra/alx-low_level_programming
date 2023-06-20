4bfac1e2b82b25e3ad73eaa2ccc022c304f1f71a
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-20 15:45:00 -0700

    Fibonacci 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the fibonacci sequences
+ *
+ * Description: The first 50 Fibonnaci numbers starting from 1 and 2
+ *
+ * Return: 0 always success;
+ */
+int main(void)
+{
+	int list[50];
+
+	list[0] = 1;
+	list[1] = 2;
+
+	int i;
+
+	for (i = 2; i < 50; i++)
+		list[i] = list[i - 1] + list[i - 2];
+
+	int j;
+
+	for (j = 0; j < 50; j++)
+	{
+		printf("%d", list[j]);
+		if (j < 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
