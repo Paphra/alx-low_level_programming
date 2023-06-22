a25faf559bc195b1e76a5223e25ded1990e51073
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-22 04:07:31 -0700

    Print more numbers upt to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers 0 to 14
+ * Description: Print the numbers 0 to 14 - 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int num;
+	int count;
+
+	for (count = 0; count <= 10; count++)
+	{
+		num = 0;
+		while (num <= 14)
+		{
+			if (num > 9)
+				_putchar('1');
+
+			_putchar('0' + (num % 10));
+			num++;
+		}
+
+		_putchar('\n');
+	}
+
+}
