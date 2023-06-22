30ceee53d9725f6c954c94cebd65b162c339ea59
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-22 02:46:47 -0700

    Find the product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - find the product of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
