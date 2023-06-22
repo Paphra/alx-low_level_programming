4d7402e9299c2326f734a6ae21df28862e6a5f89
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-22 05:23:54 -0700

    Print a square of # chars

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,16 @@ void print_square(int size)
 	int row;
 	int col;
 
-	for (row = 0; row < size; row++)
+	if (size > 0)
 	{
-		for (col = 0; col < size; col++)
-			_putchar('#');
+		for (row = 0; row < size; row++)
+		{
+			for (col = 0; col < size; col++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	} else
+	{
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
