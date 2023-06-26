7014880be14bff328213bbf239a3e97937f58eda
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-06-26 15:53:52 -0700

    Reverse String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string to reverse
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char *start = s;
+	char *end = s;
+
+	while (*end != '\0')
+	{
+		end++;
+	}
+	end--;
+
+	while (start < end)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+
+		start++;
+		end--;
+	}
+}
