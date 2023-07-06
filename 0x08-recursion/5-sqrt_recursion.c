057e46555de932b7362a3f6fbcc17a113d892a8b
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-07-06 04:19:38 -0700

    Edit - function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,25 +3,17 @@
 /**
  * helper - helping to make the square root recursion
  * @n: the number
- * @start: start guess
- * @end: ending the game
+ * @x: the first guess
  * Return: square root
  */
-int helper(int n, int start, int end)
+int helper(int n, int x)
 {
-	int mid, square;
-
-	if (start > end)
+	if (x * x == n)
+		return (x);
+	if (x * x > n)
 		return (-1);
-	mid = (start + end) / 2;
-	square = mid * mid;
 
-	if (square == n)
-		return (mid);
-	else if (square > n)
-		return (helper(n, start, mid - 1));
-	else
-		return (helper(n, mid + 1, end));
+	return (helper(n, x + 1));
 }
 
 /**
@@ -31,7 +23,9 @@ int helper(int n, int start, int end)
  */
 int _sqrt_recursion(int n)
 {
+	int x = 0;
+
 	if (n < 0)
 		return (-1);
-	return (helper(n, 0, n));
+	return (helper(n, x));
 }
