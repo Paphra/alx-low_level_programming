e5dde0f73abe92a7d1fd3a22f968597819128368
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-07-10 18:48:17 -0700

    Edit 002 - allow_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,21 +17,21 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int) * height);
-	if (grid != NULL)
+	grid = (int **)malloc(sizeof(int) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < height; i++)
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 		{
-			grid[i] = malloc(sizeof(int) * width);
-			if (grid[i] != NULL)
-			{
-				for (j = 0; j < width; j++)
-					grid[i][j] = 0;
-			} else
-			{
-				return (NULL);
-			}
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
 
 	return (grid);
