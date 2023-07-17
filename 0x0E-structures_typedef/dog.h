079a68c659fea0719947a2bdce0545f4d0de7725
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-07-17 05:26:21 -0700

    New Type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,9 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - Typedef of for struct go
+ */
+typedef struct dog dog_t;
 
 #endif /* #ifndef DOCG_H */
