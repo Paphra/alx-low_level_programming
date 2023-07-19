ccc17659f8b7fa5166a70f83e17449f815af0888
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-07-19 05:15:35 -0700

    Edit prints a name function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-/**
- * Function pointer prototypes
- */
 
-void _putchar(char);
+void _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
 #endif /* #ifndef FUNCTION_POINTERS_H */
