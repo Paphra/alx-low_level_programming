13b195043398f32e53825e35efc23cedd2cad0fa
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-07-31 19:35:38 -0700

    Edit - 9-insert_*

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *elem;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	elem = *head;
 	while (elem != NULL)
 	{
