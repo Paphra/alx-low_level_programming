25546d23f8c9cb2067d2f0e98f5f2d4a0ee3d7bb
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-08-13 18:23:00 -0700

    Changes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,76 +5,52 @@
 #include <string.h>
 #include <elf.h>
 #include "main.h"
+typedef unsigned char uc;
 
 /**
- * print_magic - prints the magic
- * @e_ident: the identity
+ * print_magic_class_data_version - prints the magic, class, data and version
+ * @h: the header
  * Return: nothing
  */
-void print_magic(unsigned char *e_ident)
+void print_magic_class_data_version(Elf64_Ehdr h)
 {
 	int i;
 
 	printf("Magic:   ");
 	for (i = 0; i < EI_NIDENT; i++)
-		printf("%02x ", e_ident[i]);
+		printf("%02x ", header.e_ident[i]);
 	printf("\n");
-}
-
-/**
- * print_class - prints the class
- * @class:the class char
- * Return: nothing
- */
-void print_class(unsigned char class)
-{
 	printf("Class:                             ");
-	switch (class) {
-		case ELFCLASS32:
-			printf("ELF32\n");
-			break;
-		case ELFCLASS64:
-			printf("ELF64\n");
-			break;
-		default:
-			printf("Unknown\n");
-			break;
+	switch (h.e_ident[EI_CLASS])
+	{
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("Unknown\n");
+		break;
 	}
-}
-
-/**
- * print_data - prints data
- * @data: char of  data type
- * Return: nothing
- */
-void print_data(unsigned char data)
-{
 	printf("Data:                              ");
-	switch (data) {
-		case ELFDATA2LSB:
-			printf("2's complement, little endian\n");
-			break;
-		case ELFDATA2MSB:
-			printf("2's complement, big endian\n");
-			break;
-		default:
-			printf("Unknown data format\n");
-			break;
+	switch (h.e_ident[EI_DATA])
+	{
+	case ELFDATA2LSB:
+		printf("2's complement, little endian\n");
+		break;
+	case ELFDATA2MSB:
+		printf("2's complement, big endian\n");
+		break;
+	default:
+		printf("Unknown data format\n");
+		break;
 	}
-}
-
-/**
- * print_version - printsthe version
- * @version: char for version
- * Return: nothing
- */
-void print_version(unsigned char version)
-{
 	printf("Version:                           ");
-	if (version == EV_CURRENT)
+	if (h.e_ident[EI_VERSION] == EV_CURRENT)
 		printf("1 (current)\n");
 	else
-		printf("%u\n", version);
+		printf("%u\n", h.e_ident[EI_VERSION]);
 }
 
 /**
@@ -85,100 +61,108 @@ void print_version(unsigned char version)
 void print_os_abi(unsigned char osabi)
 {
 	printf("OS/ABI:                            ");
-	switch (osabi) {
-		case ELFOSABI_SYSV:
-			printf("UNIX - System V\n");
-			break;
-		case ELFOSABI_HPUX:
-			printf("UNIX - HP-UX\n");
-			break;
-		case ELFOSABI_NETBSD:
-			printf("NetBSD\n");
-			break;
-		case ELFOSABI_LINUX:
-			printf("Linux\n");
-			break;
-		case ELFOSABI_SOLARIS:
-			printf("Sun Solaris\n");
-			break;
-		case ELFOSABI_AIX:
-			printf("IBM AIX\n");
-			break;
-		case ELFOSABI_IRIX:
-			printf("SGI Irix\n");
-			break;
-		case ELFOSABI_FREEBSD:
-			printf("FreeBSD\n");
-			break;
-		case ELFOSABI_TRU64:
-			printf("Compaq TRU64 UNIX\n");
-			break;
-		case ELFOSABI_MODESTO:
-			printf("Novell Modesto\n");
-			break;
-		case ELFOSABI_OPENBSD:
-			printf("OpenBSD\n");
-			break;
-		case ELFOSABI_ARM_AEABI:
-			printf("ARM EABI\n");
-			break;
-		case ELFOSABI_ARM:
-			printf("ARM\n");
-			break;
-		case ELFOSABI_STANDALONE:
-			printf("Standalone (embedded) application\n");
-			break;
-		default:
-			printf("Unknown: %u\n", osabi);
-			break;
+	switch (osabi)
+	{
+	case ELFOSABI_SYSV:
+		printf("UNIX - System V\n");
+		break;
+	case ELFOSABI_HPUX:
+		printf("UNIX - HP-UX\n");
+		break;
+	case ELFOSABI_NETBSD:
+		printf("NetBSD\n");
+		break;
+	case ELFOSABI_LINUX:
+		printf("Linux\n");
+		break;
+	case ELFOSABI_SOLARIS:
+		printf("Sun Solaris\n");
+		break;
+	case ELFOSABI_AIX:
+		printf("IBM AIX\n");
+		break;
+	case ELFOSABI_IRIX:
+		printf("SGI Irix\n");
+		break;
+	case ELFOSABI_FREEBSD:
+		printf("FreeBSD\n");
+		break;
 	}
 }
 
 /**
- * print_abi_version - prints the abi version
- * @abi_version: the version of the OS ABI
- * Return:nothing
+ * print_os_abi2 - prints the second half of the OS ABI
+ * @abi: OS ABI
+ * Return: nothing
  */
-void print_abi_version(unsigned char abi_version)
+void print_os_abi2(uc abi)
 {
-	printf("ABI Version:                       %u\n", abi_version);
+	switch(abi)
+	{
+	case ELFOSABI_TRU64:
+		printf("Compaq TRU64 UNIX\n");
+		break;
+	case ELFOSABI_MODESTO:
+		printf("Novell Modesto\n");
+		break;
+	case ELFOSABI_OPENBSD:
+		printf("OpenBSD\n");
+		break;
+	case ELFOSABI_ARM_AEABI:
+		printf("ARM EABI\n");
+		break;
+	case ELFOSABI_ARM:
+		printf("ARM\n");
+		break;
+	case ELFOSABI_STANDALONE:
+		printf("Standalone (embedded) application\n");
+		break;
+	default:
+		printf("Unknown: %u\n", osabi);
+		break;
+	}
 }
 
 /**
- * print_type: prints the type
- * @type: the type
- * Return: nothing
+ * print_abi_version_type - prints the abi version and type
+ * @abi_version: the version of the OS ABI
+ * @type: type
+ * Return:nothing
  */
-void print_type(uint16_t type) {
+void print_abi_version_type(uc abi_version, uint16_t type)
+{
+	printf("ABI Version:                       %u\n", abi_version);
+
 	printf("Type:                              ");
-	switch (type) {
-		case ET_EXEC:
-			printf("EXEC (Executable file)\n");
-			break;
-		case ET_REL:
-			printf("REL (Relocatable file)\n");
-			break;
-		case ET_DYN:
-			printf("Shared object file\n");
-			break;
-		case ET_CORE:
-			printf("Core file\n");
-			break;
-		case ET_LOOS:
-			printf("Operating system-specific range start\n");
-			break;
-		case ET_HIOS:
-			printf("Operating system-specific range end\n");
-			break;
-		case ET_LOPROC:
-			printf("Processor-specific range start\n");
-			break;
-		case ET_HIPROC:
-			printf("Processor-specific range end\n");
-			break;
-		default:
-			printf("UNKNOWN: %u\n", type);
-			break;
+	switch (type)
+	{
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
+		break;
+	case ET_DYN:
+		printf("Shared object file\n");
+		break;
+	case ET_CORE:
+		printf("Core file\n");
+		break;
+	case ET_LOOS:
+		printf("Operating system-specific range start\n");
+		break;
+	case ET_HIOS:
+		printf("Operating system-specific range end\n");
+		break;
+	case ET_LOPROC:
+		printf("Processor-specific range start\n");
+		break;
+	case ET_HIPROC:
+		printf("Processor-specific range end\n");
+		break;
+	default:
+		printf("UNKNOWN: %u\n", type);
+		break;
 	}
 }
 
@@ -227,16 +211,13 @@ int main(int argc, char *argv[])
 		close(fd);
 		exit(98);
 	}
-	
-	print_magic(header.e_ident);
-	print_class(header.e_ident[EI_CLASS]);
-	print_data(header.e_ident[EI_DATA]);
-	print_version(header.e_ident[EI_VERSION]);
+
+	print_magic_class_data_version(header);
 	print_os_abi(header.e_ident[EI_OSABI]);
-	print_abi_version(header.e_ident[EI_ABIVERSION]);
-	print_type(header.e_type);
+	print_os_abi2(header.e_ident[EI_OSABI]);
+	print_abi_version_type(header.e_ident[EI_ABIVERSION], header.e_type);
 	print_entry(header.e_entry);
 
 	close(fd);
-	return 0;
+	return (0);
 }
