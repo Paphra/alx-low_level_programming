5c8c1ef73058163ce8c2635b02856d82ef03fe21
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-08-14 05:13:50 -0700

    Digito

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -163,7 +163,7 @@ void print_abi_version_type_entry(Elf64_Ehdr h)
 		printf("UNKNOWN: %u\n", h.e_type);
 		break;
 	}
-	printf("  Entry point address:               0x%.8lx\n", entry);
+	printf("  Entry point address:               0x%lx\n", entry);
 }
 
 /**
