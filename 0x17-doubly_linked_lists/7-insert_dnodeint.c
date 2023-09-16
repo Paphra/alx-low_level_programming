24b5f796419627c1fa000bff9540ac890949f0eb
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-16 23:00:00 +0000

    Edits of 7-insert 003

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *tmp;
+	dlistint_t *new = NULL, *tmp = NULL;
 	unsigned int count = 0;
 
 	if (h == NULL)
