21ad205afbb0ad3089a63c63ae97e04d79e2ada5
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-16 23:24:59 +0000

    somes 005

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,8 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = NULL, *tmp = NULL;
 	unsigned int count = 0;
 
-	if (h == NULL)
-		return (NULL);
 	new =  (dlistint_t *) malloc(sizeof(dlistint_t *));
 	if (new == NULL)
 		return (NULL);
